9f3d0d1e2a37e1772dc1cf0887b1efa9ea91cb1b
naderalhamwi <38947594+naderalhamwi@users.noreply.github.com>
2020-08-24 15:34:04 +0200

    33
    java

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -3,8 +3,8 @@ let myJSON = '{"name":" Nader ","age":" 22 "}';
 
  students = {student:[
     {"elevID":1, "Firstname":"Nader", "lastname":"Alahmwi", "age":22},
-    {"elevID":1, "Firstname":"tim", "lastname":"tuvvestam", "age":28},
-    {"elevID":1, "Firstname":"David", "lastname":"werrnow", "age":28}
+    {"elevID":1, "Firstname":"tim", "lastname":"tuvestam", "age":28},
+    {"elevID":1, "Firstname":"David", "lastname":"wernow", "age":28}
  ]}
 
 let text = document.getElementById("text");
@@ -21,3 +21,28 @@ let text = document.getElementById("text");
    document.getElementById("fname3").innerHTML = students.student[2].Firstname;
    document.getElementById("lname3").innerHTML = students.student[2].lastname;
    document.getElementById("age3").innerHTML = students.student[2].age;
+
+
+function intext (){
+   var table = document.getElementById("myTable");
+
+   let Fname = document.getElementById("fname").value;
+   let Lname = document.getElementById("lnale").value;
+   let Age = document.getElementById("Age").value;
+
+   var row = table.insertRow(0);
+   var row = table.inserlRow(0);
+   var row = table.inserAge(0);
+   var cell1 = row.insertCell(0);
+   var cell2 = row.insertCell(1);
+   var cell3 = row.insertCell(2);
+
+   cell1.innerHTML = Fname;
+   cell2.innerHTML = Lname;
+   cell3.innerHTML = Age;
+}
+
+
+function Uttext(){
+  document.getElementById("myTable").deleteRow(0);
+}
