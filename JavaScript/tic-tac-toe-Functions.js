01aa62782004334ca4350c6c60a2da2ff9dfdfcd
Naderalhawmi <45594567+Naderalhawmi@users.noreply.github.com>
2020-09-01 22:23:49 +0200

    Tic-Tac-Toe 0.2
    man nu kan vinna
    byte lite på färger

--- JavaScript/tic-tac-toe-Functions.js
+++ JavaScript/tic-tac-toe-Functions.js
@@ -1,69 +1,206 @@
-//rätt
 function B1(){
     if(player == 1){
-        if(cell1.value == 1)
-        {
-            console.log("du få inte");
+        if(cell1.value == 0){
+            cell1.value = 1;
+            cell1.style.backgroundColor = purble;
+            playerO();
         }else{
-            cell1.style.backgroundColor = gren;
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell1.value == 0){
+            cell1.value = 2;
+            cell1.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
         }
     }
 }
 function B2(){
     if(player == 1){
-        cell2.style.backgroundColor = gren;
-    }else{
-        cell2.style.backgroundColor = "tomato";
+        if(cell2.value == 0){
+            cell2.value = 1;
+            cell2.style.backgroundColor = purble;
+            playerO();
+        }else{
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell2.value == 0){
+            cell2.value = 2;
+            cell2.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
+        }
     }
 }
 function B3(){
     if(player == 1){
-        cell3.style.backgroundColor = gren;
-    }else{
-        cell3.style.backgroundColor = "tomato";
+        if(cell3.value == 0){
+            cell3.value = 1;
+            cell3.style.backgroundColor = purble;
+            playerO();
+        }else{
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell3.value == 0){
+            cell3.value = 2;
+            cell3.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
+        }
     }
 }
 function B4(){
     if(player == 1){
-        cell4.style.backgroundColor = gren;
-    }else{
-        cell4.style.backgroundColor = "tomato";
+        if(cell4.value == 0){
+            cell4.value = 1;
+            cell4.style.backgroundColor = purble;
+            playerO();
+        }else{
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell4.value == 0){
+            cell4.value = 2;
+            cell4.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
+        }
     }
 }
 function B5(){
     if(player == 1){
-        cell5.style.backgroundColor = gren;
-    }else{
-        cell5.style.backgroundColor = "tomato";
+        if(cell5.value == 0){
+            cell5.value = 1;
+            cell5.style.backgroundColor = purble;
+            playerO();
+        }else{
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell5.value == 0){
+            cell5.value = 2;
+            cell5.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
+        }
     }
 }
 function B6(){
     if(player == 1){
-        cell6.style.backgroundColor = gren;
-    }else{
-        cell6.style.backgroundColor = "tomato";
+        if(cell6.value == 0){
+            cell6.value = 1;
+            cell6.style.backgroundColor = purble;
+            playerO();
+        }else{
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell6.value == 0){
+            cell6.value = 2;
+            cell6.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
+        }
     }
 }
 function B7(){
     if(player == 1){
-        cell7.style.backgroundColor = gren;
-    }else{
-        cell7.style.backgroundColor = "tomato";
+        if(cell7.value == 0){
+            cell7.value = 1;
+            cell7.style.backgroundColor = purble;
+            playerO();
+        }else{
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell7.value == 0){
+            cell7.value = 2;
+            cell7.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
+        }
     }
 }
 function B8(){
     if(player == 1){
-        cell8.style.backgroundColor = gren;
-    }else{
-        cell8.style.backgroundColor = "tomato";
+        if(cell8.value == 0){
+            cell8.value = 1;
+            cell8.style.backgroundColor = purble;
+            playerO();
+        }else{
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell8.value == 0){
+            cell8.value = 2;
+            cell8.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
+        }
     }
 }
 function B9(){
     if(player == 1){
-        cell9.style.backgroundColor = gren;
-    }else{
-        cell9.style.backgroundColor = "tomato";
+        if(cell9.value == 0){
+            cell9.value = 1;
+            cell9.style.backgroundColor = purble;
+            playerO();
+        }else{
+            alert("Redan valt");
+        }
+    }else if(player == 2){
+        if(cell9.value == 0){
+            cell9.value = 2;
+            cell9.style.backgroundColor = darkpurple;
+            playerX();
+        }else{
+            alert("Redan valt");
+        }
     }
 }
 
+function checwin(){
+    // chek vertikala
+    if(cell1.value == player && cell2.value == player && cell3.value == player){
+        alert("player " + player + " vann");
+        return location.reload();
+    }else if(cell4.value == player && cell5.value == player && cell6.value == player){
+        alert("player " + player + " vann");
+        return location.reload();
+    }else if(cell7.value == player && cell8.value == player && cell9.value == player){
+        alert("player " + player + " vann");
+        return location.reload();
+    }
+    //chek horisontella
+    else if(cell1.value == player && cell4.value == player && cell7.value == player){
+        alert("player " + player + " vann");
+        return location.reload();
 
+    }else if(cell2.value == player && cell5.value == player && cell8.value == player){
+        alert("player " + player + " vann");
+        return location.reload();
+
+    }else if(cell3.value == player && cell6.value == player && cell9.value == player){
+        alert("player " + player + " vann");
+        return location.reload();
+    }
+    //chek streak / \
+    else if(cell1.value == player && cell5.value == player && cell9.value == player){
+        alert("player " + player + " vann");
+        return location.reload();
+    }else if(cell3.value == player && cell5.value == player && cell7.value == player){
+        alert("player " + player + " vann");
+        return location.reload();
+    }
+}
--- JavaScript/tic-tac-toe.js
+++ JavaScript/tic-tac-toe.js
@@ -1,7 +1,9 @@
 let player;
 let cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9; 
 
-let gren = "springgreen";
+let purble = "#ff1aff";
+let darkpurple = "#3d0099";
+
 
 function init(){
 
@@ -15,40 +17,65 @@ function init(){
     cell8 = document.getElementById("Cell8");
     cell9 = document.getElementById("Cell9");
 
+    cell1.value = 0;
+    cell2.value = 0;
+    cell3.value = 0;
+    cell4.value = 0;
+    cell5.value = 0;
+    cell6.value = 0;
+    cell7.value = 0;
+    cell8.value = 0;
+    cell9.value = 0;
+
     document.getElementById("sliderBottom").style.opacity = "1";
     document.getElementById("sliderBottom").style.opacity = "1";
     document.getElementById("sliderBottom").style.height = "660px";
     document.getElementById("sliderBottom").style.marginTop = "310px";
-    document.getElementById("sliderBottom").style.pointerEvents = "none";
 
     document.getElementById("playGround").style.display = "none";
 }
 window.onload = init;
 
 function playerX(){
+    checwin();
     player = 1;
     document.getElementById("main").style.transition = "all 1s";
     document.getElementById("main").style.backgroundColor = "#393939";
     
+    document.getElementById("sliderBottom").innerHTML = "<h1>" + "Player 1" + "</h1>";
+    
     document.getElementById("sliderBottom").style.height = "310px";
+    document.getElementById("sliderBottom").style.color = "white";
     document.getElementById("sliderBottom").style.marginTop = "0px";
     document.getElementById("sliderBottom").style.transition = "all 1.5s";
-    document.getElementById("sliderBottom").style.backgroundColor = gren;
+    document.getElementById("sliderBottom").style.backgroundColor = purble;
     document.getElementById("sliderBottom").style.boxShadow = "0px 5px 10px black";
+    
+    document.getElementById("sliderBottom").style.pointerEvents = "none";
+    document.getElementById("tic").style.pointerEvents = "none";
+    document.getElementById("tac").style.pointerEvents = "none";
     
     document.getElementById("playGround").style.display = "block";
 }
 
 function playerO(){
-    player = 0;
+    checwin();
+    player = 2;
     document.getElementById("main").style.transition = "all 1s";
     document.getElementById("main").style.backgroundColor = "#393939";
-
+    
+    document.getElementById("sliderBottom").innerHTML = "<h1>" + "Player 2" + "</h1>";
+    
     document.getElementById("sliderBottom").style.height = "310px";
+    document.getElementById("sliderBottom").style.color = "white";
     document.getElementById("sliderBottom").style.marginTop = "0px";
     document.getElementById("sliderBottom").style.transition = "all 1.5s";
-    document.getElementById("sliderBottom").style.backgroundColor = "tomato";
+    document.getElementById("sliderBottom").style.backgroundColor = darkpurple;
     document.getElementById("sliderBottom").style.boxShadow = "0px 5px 10px black";
-
+    
+    document.getElementById("sliderBottom").style.pointerEvents = "none";
+    document.getElementById("tic").style.pointerEvents = "none";
+    document.getElementById("tac").style.pointerEvents = "none";
+    
     document.getElementById("playGround").style.display = "block";
 }
